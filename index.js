175299f5ade8780aed91c041766ee85dc78beecc
Eralp Karaduman <eralp@eralpkaraduman.com>
2020-12-29 22:49:57 +0200

    logging

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const fetch = require('node-fetch');
 const { send } = require('process');
+const bunyan = require('bunyan');
+const { LoggingBunyan } = require('@google-cloud/logging-bunyan');
+
+const loggingBunyan = new LoggingBunyan();
+
+const logger = bunyan.createLogger({
+  name: 'kulakshot-telegram-bot',
+  streams: [
+    { stream: process.stdout, level: 'info' },
+    loggingBunyan.stream('info'),
+  ],
+});
+
 
 const sendToChat = (chatId, text) => {
   fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(text)}`)
     .then(res => res.text())
-    .then(body => console.log(body));
+    .then(body => logger.info(body));
 }
 
 const BUZZSPROUT_PODCAST_ID = 1319524
@@ -18,7 +31,7 @@ const fetchPodcastEpisodes = () =>
     }
   })
     .then((res) => res.json())
-    .catch(console.error.bind(console));
+    .catch(err => logger.error(err));
 
 
 const createMessageResponse = (text) => {
@@ -49,7 +62,7 @@ const handler = (req, res) => {
     return res.status(204).send('');
   } else {
     try {
-      console.log(JSON.stringify(req.body))
+      logger.info(req.body)
       let text = req.body.text
       let chat = req.body.chat
       const message = req.body.message || {}
@@ -61,7 +74,7 @@ const handler = (req, res) => {
         sendToChat(chat.id, 'ne?');
       }
     } catch (e) {
-      console.log(e)
+      logger.error(e)
       return res.status(500).send('FAILED')
     }
     return res.status(200).send('OK');
