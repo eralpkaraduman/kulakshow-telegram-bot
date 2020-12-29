8e79d2e9ef0c864bb2c2b923cc6865a16bbad584
Eralp Karaduman <eralp@eralpkaraduman.com>
2020-12-29 20:13:25 +0200

    fetch

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-const rp = require('request-promise');
+const fetch = require('node-fetch')
 
 const sendToChat = (chat_id, text) => {
-  return rp({
-    method: 'GET',
-    uri: `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
-    qs: {
-      chat_id,
-      text
-    }
-  })
+  fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage?chat_id=${chat_id}&text=${text}`)
+    .then(res => res.text())
+    .then(body => console.log(body))
 }
 
 
@@ -27,12 +22,14 @@ const handler = (req, res) => {
         sendToChat(req.body.message.chat.chat_id, req.body.message.text);
       }
     } catch (e) {
-      return res.status(500).send(JSON.stringify(e))
+      console.log(e)
+      return res.status(500).send('FAILED')
     }
     return res.status(200).send('OK');
   }
 
 }
 
+sendToChat('aaa1', 'aaaa2')
 
 exports.telegram = handler;
